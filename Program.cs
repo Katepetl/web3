fea4975b990fb1e0e270453152f1c1ef55918f27
Kate <petl@bk.ru>
2022-10-24 23:36:56 +0400

    added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vvedite chislo ");
+string writeN=Console.ReadLine()!;
+int N=Convert.ToInt32(writeN);
+int[] GetKub(int N)
+{
+    int[] Kubarray = new int [N];
+    int number=1;
+    for (int i= 0; i<N; i++, number++)
+    {
+        Kubarray [i] = (int)Math.Pow(i+1,3);
+        number++;
+    }
+    return Kubarray;
+}
+void PrintTable(int[] Array)
+{
+    int number=1;
+    for (int i=0; i<Array.Length; i++)
+    {
+    System.Console.Write($"{number} v kube=");
+    System.Console.WriteLine(Array[i]);
+    number++;
+    }
+}
+
+int[]Kubarray=GetKub(N);
+PrintTable(Kubarray);
---
+++ Z2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Vvedite koordinati pervoi tochki: x,y,z. Vvedite x");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Vvedite y");
+int y=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Vvedite z");
+int z=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Vvedite koordinati vtoroi tochki: x1,y1,z1. Vvedite x1");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Vvedite y1");
+int y1=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Vvedite z1");
+int z1=int.Parse(Console.ReadLine()!);
+double GetSize(int x,int x1,int y, int y1, int z, int z1)
+{
+double distance=Math.Sqrt(Math.Pow(x1-x,2)+Math.Pow(y1-y,2)+Math.Pow(z1-z,2));
+return distance;
+}
+double distance= GetSize(x,x1,y,y1,z,z1);
+Console.WriteLine(distance);
